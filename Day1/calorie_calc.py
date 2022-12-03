1ddd2d3793aee620da8b49e4d57d7620e8617cf2
Sam <77520156+davisssamuel@users.noreply.github.com>
2022-12-03 12:57:05 -0500

    Add files via upload

---
+++ Day1/calorie_calc.py
@@ -0,0 +1,17 @@
+# Part 1
+index = 0
+total_calories = list()
+with open("input.txt") as infile:
+    data = infile.readlines()
+    total = 0
+    for line in data:
+        if line != '\n':
+            total += int(line)
+        else:
+            total_calories.append(total)
+            total = 0
+print(f"MAX: {max(total_calories)}")
+
+# Part 2
+sum = sum(sorted(total_calories)[len(total_calories) - 3:len(total_calories)])
+print(f"TOTAL OF TOP THREE: {sum}")
